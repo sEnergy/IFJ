6cb153cfe532d688027e63661b3f9fe984537697
White-dracke <vanicekl@gmail.com>
2013-10-28 23:34:14 +0100

    tabulators

--- lex_analyzer.c
+++ lex_analyzer.c
@@ -3,9 +3,9 @@
  * Project name:        IFJ - Team project
  * Filename:            lex_analyzer.c
  * Author:              Marcel Fiala
- * 						Luboš Vaníček
- * 						Matúš Turic
- * 						Bambi
+ *                      Luboš Vaníček
+ *                      Matúš Turic
+ *                      Bambi
  * Encoding:            UTF-8
  *
  * Description:         Source file  of Lexical analyzer for team project
@@ -343,34 +343,34 @@ int lex_analyzer (FILE *input, int *token_id, BUFFER_STRUCT buffer)
                         return 0;
                     }
                 } break;
-			/*
+            /*
              * STATE_VARIABLE
              */
             case '$':
-				{
+                {
 					c = fgetc(input);
 					if (isalpha(c) || c == '_')
 					{
 						write_c(buffer,c);
 						c = fgetc(input);
 						while (isalpha(c) || isdigit(c) || c == '_')
-						{
-							write_c(buffer,c);
-							c = fgetc(input);
-						}
-						ungetc(c, input);
-						*token_id = IFJ_T_VARIALBE;
-						return 0;
-					}
-					else
-					{
-						return IFJ_ERR_LEXICAL;
-					}
+                        {
+                            write_c(buffer,c);
+                            c = fgetc(input);
+                        }
+                        ungetc(c, input);
+                        *token_id = IFJ_T_VARIALBE;
+                        return 0;
+                    }
+                    else
+                    {
+                        return IFJ_ERR_LEXICAL;
+                    }
                 } break;
             case '/':
                 {
                     c = fgetc(input);
-
+    
                     if (c == '/') // line comment
                     {
                         while (1) // only EOF or newline end the cycle
