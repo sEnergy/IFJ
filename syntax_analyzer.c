1f97ec44aa6e86df5a82a74f988dfa825ada6d3a
strakai <is.straka@gmail.com>
2013-11-28 01:53:23 +0100

    Changed intends.

--- syntax_analyzer.c
+++ syntax_analyzer.c
@@ -45,80 +45,80 @@ int work(Stack_t* stack, TokenList* list)
     int i = 1;
     while(proceeded != NULL && proceeded->content != S_top(stack))
     {
-    	i++;
-    	proceeded = proceeded->LPtr;
-    	if (i>=6)
-    	{
-    		return IFJ_ERR_LEXICAL;
-    	}
+        i++;
+        proceeded = proceeded->LPtr;
+        if (i>=6)
+        {
+            return IFJ_ERR_LEXICAL;
+        }
     }
     proceeded = list->active->LPtr;
     if (proceeded->content->id == IFJ_T_RB)
     {    
-    	if (i!=5)
-    	{
-    		return IFJ_ERR_LEXICAL;
-    	}
-    	List_itemPtr r_op = proceeded->LPtr;
-    	List_itemPtr operator = r_op->LPtr;
-    	List_itemPtr l_op = r_op->LPtr;
-    	List_itemPtr l_br = r_op->LPtr;
-    	if (is_terminal(r_op->content->id) && l_br->content->id == IFJ_T_LB
-    		&& is_terminal(l_op->content->id) && is_operator(operator->content->id))
-    	{
-    		operator->content->LPtr = l_op->content;
-    		operator->content->RPtr = r_op->content;
-    		operator->LPtr = l_br->LPtr;
-    		operator->RPtr = proceeded->RPtr;
-    		proceeded->RPtr->LPtr = operator;
-    		l_br->LPtr->RPtr = operator;
-    		free(l_op);
-    		free(r_op);
-    		free(proceeded);
-    		free(l_br);
+        if (i!=5)
+        {
+            return IFJ_ERR_LEXICAL;
+        }
+        List_itemPtr r_op = proceeded->LPtr;
+        List_itemPtr operator = r_op->LPtr;
+        List_itemPtr l_op = r_op->LPtr;
+        List_itemPtr l_br = r_op->LPtr;
+        if (is_terminal(r_op->content->id) && l_br->content->id == IFJ_T_LB
+            && is_terminal(l_op->content->id) && is_operator(operator->content->id))
+        {
+            operator->content->LPtr = l_op->content;
+            operator->content->RPtr = r_op->content;
+            operator->LPtr = l_br->LPtr;
+            operator->RPtr = proceeded->RPtr;
+            proceeded->RPtr->LPtr = operator;
+            l_br->LPtr->RPtr = operator;
+            free(l_op);
+            free(r_op);
+            free(proceeded);
+            free(l_br);
             S_pop(stack);
             S_pop(stack);
             S_pop(stack);
-    	}
-    	else
-    	{
-    		return IFJ_ERR_LEXICAL;
-    	}
+        }
+        else
+        {
+            return IFJ_ERR_LEXICAL;
+        }
     }
     else if (is_terminal(proceeded->content->id))
     {
-    	if (i!=3)
-    	{
-    		return IFJ_ERR_LEXICAL;
-    	}
-    	List_itemPtr operator = proceeded->LPtr;
-    	List_itemPtr l_op = operator->LPtr;
-    	if (is_terminal(proceeded->content->id) && 
-    		is_operator(operator->content->id))
-    	{
-    		operator->content->LPtr = l_op->content;
-    		operator->content->RPtr = proceeded->content;
-    		operator->LPtr = l_op->LPtr;
-    		operator->RPtr = proceeded->RPtr;
-    		proceeded->RPtr->LPtr = operator;
-    		l_op->LPtr->RPtr = operator;
-    		free(l_op);
-    		free(proceeded);
+        if (i!=3)
+        {
+            return IFJ_ERR_LEXICAL;
+        }
+        List_itemPtr operator = proceeded->LPtr;
+        List_itemPtr l_op = operator->LPtr;
+        if (is_terminal(proceeded->content->id) && 
+            is_operator(operator->content->id))
+        {
+            operator->content->LPtr = l_op->content;
+            operator->content->RPtr = proceeded->content;
+            operator->LPtr = l_op->LPtr;
+            operator->RPtr = proceeded->RPtr;
+            proceeded->RPtr->LPtr = operator;
+            l_op->LPtr->RPtr = operator;
+            free(l_op);
+            free(proceeded);
             S_pop(stack);
-    	}
-    	else
-    	{
-    		return IFJ_ERR_LEXICAL;
-    	}
+        }
+        else
+        {
+            return IFJ_ERR_LEXICAL;
+        }
     }
     else
     {
-    	return IFJ_ERR_LEXICAL;
+        return IFJ_ERR_LEXICAL;
     }
     return 0;
 }
-    	
-    			
+        
+                
     
 int PSA(TokenList* list)
 {
@@ -152,12 +152,12 @@ int PSA(TokenList* list)
     };
     if((start = new_token()) == NULL)
     {
-    	return IFJ_ERR_INTERNAL;
+        return IFJ_ERR_INTERNAL;
     }
     if((end = new_token()) == NULL)
     {
-    	free(end);
-    	return IFJ_ERR_INTERNAL;
+        free(end);
+        return IFJ_ERR_INTERNAL;
     }
     start->id = IFJ_T_MOD;
     end->id = IFJ_T_MOD;
@@ -171,21 +171,21 @@ int PSA(TokenList* list)
     
     while (!(S_top(&stack) != start && TL_GetID(list) != end))
     {
-    	TokenPtr input = TL_GetID(list);
+        TokenPtr input = TL_GetID(list);
         if (is_terminal(input->id)) 
         {
             TL_ActiveNext(list); // simulacia E -> i
             continue;
         }
-    	stack_top = S_top(&stack);
+        stack_top = S_top(&stack);
         switch (table[stack_top->id-2][list->active->content->id-2])
         {
             
             case 4:
                 break;
             case 3:
-    			TL_ActiveNext(list);
-    			break;
+                TL_ActiveNext(list);
+                break;
             case 2:
                 if ((code = work(&stack,list)) != 0)
                 {
